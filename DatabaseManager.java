453f51c7ae704c75edfd03954c35bf8529fa77a0
ALonelyLoner <62716237+ALonelyLoner@users.noreply.github.com>
2021-03-04 17:57:22 -0600

    This shit works???

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -5,10 +5,12 @@ public class DatabaseManager {
 
     private Connection connect() {
         // SQLite connection string
-        String url = "jdbc:sqlite:C:test/test.db";
+        // C:\Users\tjoli\OneDrive\Documents\GitHub\ESGapplication\Customer.db
+        String url = "jdbc:sqlite:C:\\Users\\tjoli\\OneDrive\\Documents\\GitHub\\ESGapplication\\Customer.db";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
+            System.out.println("Connection Successful\n");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -30,9 +32,34 @@ public class DatabaseManager {
             System.out.println(e.getMessage());
         }
     }
+
+    public String select_Users(String username) {
+        // SQL Statement
+        String sql = "SELECT * FROM customer";
+
+        try (Connection connection = this.connect()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            StringBuilder output = new StringBuilder();
+            output.append("Username,").append("\t").append("Password \n");
+
+            while (resultSet.next()) {
+                output.append(resultSet.getString("Username")).append("\t").append("\t").append(resultSet.getString("Password")).append("\n");
+                System.out.println(output.toString());
+            }
+            return output.toString();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+
     public static void main(String[] args) {
         DatabaseManager app = new DatabaseManager();
-        app.Test();
+        // app.Test();
+        app.select_Users("test");
     }
 }
 
